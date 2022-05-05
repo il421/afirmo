a83c2597c1b75a4db5723c78f59aaf1b8db4aa38
Ilya Suglobov <ilya.suglobov@bpsoftware.net>
2022-05-06 11:40:42 +1200

    fixed overflow

--- pages/business-profile.tsx
+++ pages/business-profile.tsx
@@ -1,9 +1,8 @@
-import { ProfileHeader } from "../layouts/profile/header/ProfileHeader";
 import { ProfileColumnsWrapper } from "../layouts/profile/profileColumn/ProfileColumnsWrapper";
 
 const BusinessProfile = () => {
   return (
-    <section className="h-full flex flex-col">
+    <section className="h-full flex flex-col overflow-y-auto">
       <ProfileColumnsWrapper />
     </section>
   );
